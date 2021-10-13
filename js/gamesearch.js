9fd0052bde8e17aa51691df8189f449a251903ca
RefinedSnack <refinedsnack@gmail.com>
2021-10-12 19:56:24 -0600

    Added the game search functionality

--- js/gamesearch.js
+++ js/gamesearch.js
@@ -1,11 +1,47 @@
-const CLIENT_ID = "0DFaMhGHCJ";
+var database_url = "/game-database.json";
+var gameList;
+var gameTable;
+fetch(database_url).then(function(response) {
+    if (response.status != 200) {
+        return {
+          text: "Error Loading the list of games: " + response.statusText
+        }
+      }
+    return response.json(); 
+}).then(function(json) {
+  // update DOM with response
+  gameList = json.gameList;
+  gameTable = json.gameIdTable;
+  updateResult(json.text);
+});
+
+function lookup(name) {
+    for (let i = 0; i < gameList.length; i++) {
+        //console.log(gameList[i]);
+        var stripped = name.replace(/\s+/g, '');
+        stripped = stripped.toLowerCase();
+        //console.log(stripped);
+        if ((gameList[i].toLowerCase() === stripped)) {
+            return gameTable[i].gameId;
+        }
+    }
+    return "not found";
+}
 
 function onClick(e) {
+    let gameWanted = document.getElementById('gameInput').value;
     e.preventDefault();
-    
+
+    //ensure that game is in bounds
+    var gameID = lookup(gameWanted);
+    if (gameID === "not found") {
+        console.log(gameWanted, gameID);
+        suggestGames(gameWanted);
+        return "try again";
+    }
+
     // setup URL
-    let url = "https://api.boardgameatlas.com/api/" + "search?name=Catan&client_id=" + CLIENT_ID;
-    url = "https://api.boardgameatlas.com/api/search?name=Catan&client_id=0DFaMhGHCJ"
+    url = "https://bgg-json.azurewebsites.net/thing/" + gameID;
     // call API
     fetch(url)
       .then(function(response) {
@@ -15,16 +51,38 @@ function onClick(e) {
             text: "Error calling the API service: " + response.statusText
           }
         }
+        return response.json(); 
       }).then(function(json) {
-        // update DOM with response
-        updateResult(json.text);
-      });
-  }
-  
-  function updateResult(info) {
-    document.getElementById('result').textContent = info;
-  }
-  window.onload=function(){
-    document.getElementById("gameSubmit").addEventListener("click", onClick);
+        updateResult(json);
+      });      
 }
   
+//TODO add the stuff here. Info
+  function updateResult(info) {
+    console.log(info);
+    var result = "";
+    document.getElementById('result').innerHTML = result;
+  }
+
+
+  window.onload=function(){
+    document.getElementById("gameSubmit").addEventListener("click", onClick);
+  }
+
+  function suggestGames(guess) {
+      var result = "";
+      result += "<center><p>" + guess + " wasn't a valid game title, try one of these instead!</p></center>";
+      result += "<center><table style=\"border-collapse: unset;\">";
+      const NUM_COLS = 3;
+      for (let i = 0; i < gameList.length; i += NUM_COLS) {
+          result += "<tr>";
+          for (let j = 0; j < NUM_COLS; j++) {
+              if (i+j < gameList.length)
+                result += "<td>"+gameList[i+j]+"</td>";
+            }
+            result += "</tr>";
+      }
+      result += "</center></table>";
+
+      document.getElementById('result').innerHTML = result;
+  }
